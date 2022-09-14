8304af43a60c24efb50e2f02c2561c0aab3876ca
Adam DePaolo <amdepaolo@gmail.com>
2022-09-14 14:21:42 -0400

    commit tree 35962f60f23b7b77194e43acefc3de39f777585a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    arr.map{|name| badge_maker(name)}
+end
+
+def assign_rooms(arr)
+    arr.each_with_index.map{|name, room| "Hello, #{name}! You'll be assigned to room #{room + 1}!"}
+end
+
+def printer(arr)
+    batch_badge_creator(arr).each{|badge| puts badge}
+    assign_rooms(arr).each{|message| puts message}
+end
